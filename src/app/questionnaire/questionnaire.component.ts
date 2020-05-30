650ab9bdf167919c1c80ba6a70bbaff7206ad0c4
Jennifer Plank <jenplank@rogers.com>
2020-05-30 14:18:32 -0400

    before updating because it told me to commit or stash first mine has Questionnaire and Dashboard

---
+++ src/app/questionnaire/questionnaire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnare',
+  templateUrl: './questionnaire/questionnaire.component.html',
+  styleUrls: ['./questionnaire/questionnaire.component.css']
+})
+export class QuestionnaireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
